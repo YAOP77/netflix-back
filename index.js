62a83ee56982c9ecb61b4d82b1f5c971d50a508c
YAOP77 <yaop77@gmail.com>
2025-07-03 01:31:08 +0000

    back

--- index.js
+++ index.js
@@ -9,9 +9,24 @@ const app = express();
 // Configuration Mongoose pour éviter le warning
 mongoose.set('strictQuery', false);
 
+// Debug des variables d'environnement
+console.log('=== Debug des variables d\'environnement ===');
+console.log('MONGO_URI:', process.env.MONGO_URI ? 'Définie' : 'Non définie');
+console.log('FRONTEND_URI:', process.env.FRONTEND_URI ? 'Définie' : 'Non définie');
+console.log('PORT:', process.env.PORT || 'Non définie (utilisera 5000)');
+
+if (process.env.MONGO_URI) {
+  console.log('MONGO_URI commence par:', process.env.MONGO_URI.substring(0, 20) + '...');
+} else {
+  console.log('⚠️  MONGO_URI n\'est pas définie dans les variables d\'environnement');
+}
+console.log('==========================================');
+
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://yaoyaopascal77:gJOgYVKc7YoSYlls@cluster0.ouslcep.mongodb.net/netflix-clone?retryWrites=true&w=majority&appName=Cluster0';
 const FRONTEND_URI = process.env.FRONTEND_URI || 'https://netflix-front-kappa.vercel.app';
 const PORT = process.env.PORT || 5000;
+
+console.log('URI MongoDB utilisée:', MONGO_URI.substring(0, 30) + '...');
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
