65885a16deafc997218898c6ceb17633999a8d42
YAOP77 <yaop77@gmail.com>
2025-07-05 08:47:23 +0000

    back

--- debug-env.js
+++ debug-env.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-console.log('=== Debug des variables d\'environnement ===');
-console.log('MONGO_URI:', process.env.MONGO_URI ? 'Définie' : 'Non définie');
-console.log('FRONTEND_URI:', process.env.FRONTEND_URI ? 'Définie' : 'Non définie');
-console.log('PORT:', process.env.PORT || 'Non définie (utilisera 5000)');
+// console.log('=== Debug des variables d\'environnement ===');
+// console.log('MONGO_URI:', process.env.MONGO_URI ? 'Définie' : 'Non définie');
+// console.log('FRONTEND_URI:', process.env.FRONTEND_URI ? 'Définie' : 'Non définie');
+// console.log('PORT:', process.env.PORT || 'Non définie (utilisera 5000)');
 
-if (process.env.MONGO_URI) {
-  console.log('MONGO_URI commence par:', process.env.MONGO_URI.substring(0, 20) + '...');
-} else {
-  console.log('⚠️  MONGO_URI n\'est pas définie dans les variables d\'environnement');
-}
+// if (process.env.MONGO_URI) {
+//   console.log('MONGO_URI commence par:', process.env.MONGO_URI.substring(0, 20) + '...');
+// } else {
+//   console.log('⚠️  MONGO_URI n\'est pas définie dans les variables d\'environnement');
+// }
 
-console.log('=========================================='); 
+// console.log('=========================================='); 
